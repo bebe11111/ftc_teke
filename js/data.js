3df12977f8b3b5c587749841c92b48b0c33c4391
Bor Bence <132979209+bebe11111@users.noreply.github.com>
2025-04-25 18:34:23 +0200

    data.js_fillup

--- js/data.js
+++ js/data.js
@@ -2,14 +2,175 @@ const players = [
     {
         name: 'Bodó Bettina',
         best: '',
-        gender: '',
-        image: '',
+        gender: 'nő',
+        tittle: 'jétékos',
+        img: 'https://www.fradi.hu/wm/upload/295x393/6/nr/hh647.jpg',
     },
+
+    {
+        name: 'Bognár Barbara',
+        best: '',
+        gender: 'nő',
+        img: 'https://www.fradi.hu/wm/upload/295x393/z/a4/0li6u.jpg',
+    },
+
+    {
+        name: 'Fábián Adelin',
+        best: '',
+        gender: 'nő',
+        tittle: 'jétékos',
+        img: 'https://www.fradi.hu/wm/upload/295x393/u/01/1vbia.jpg',
+    },
+
+    {
+        name: 'Heimann Ibolya',
+        best: '',
+        gender: 'nő',
+        tittle: 'jétékos',
+        img: 'https://www.fradi.hu/wm/upload/295x393/9/r8/9xmb6.jpg',
+    },
+
+    {
+        name: 'Mátraháziné Kiss Julianna',
+        best: '',
+        gender: 'nő',
+        tittle: 'jétékos',
+        img: 'https://www.fradi.hu/wm/upload/295x393/z/xu/z93g6.jpg',
+    },
+
+    {
+        name: 'Nagy Beatrix',
+        best: '',
+        gender: 'nő',
+        tittle: 'jétékos',
+        img: 'https://www.fradi.hu/wm/upload/295x393/g/hg/v8b2f.jpg',
+    },
+
+    {
+        name: 'Pintér Barbara',
+        best: '',
+        gender: 'nő',
+        tittle: 'jétékos',
+        img: 'https://www.fradi.hu/wm/upload/295x393/f/w9/0xdu5.jpg',
+    },
+
+    {
+        name: 'Sinka Cintia Gréta',
+        best: '',
+        gender: 'nő',
+        tittle: 'jétékos',
+        img: 'https://www.fradi.hu/wm/upload/295x393/e/58/2rcqs.jpg',
+    },
+
+    {
+        name: 'Szabó Mónika',
+        best: '',
+        gender: 'nő',
+        tittle: 'jétékos',
+        img: 'https://www.fradi.hu/wm/upload/295x393/j/70/vdz7y.jpg',
+    },
+
+    {
+        name: 'Tóth Andrea',
+        best: '',
+        gender: 'nő',
+        tittle: 'jétékos',
+        img: 'https://www.fradi.hu/wm/upload/295x393/2/sx/qf36f.jpg',
+    },
+
+    {
+        name: 'Bánhegyi János',
+        best: '',
+        gender: 'férfi',
+        tittle: 'jétékos',
+        img: 'https://www.fradi.hu/wm/upload/295x393/j/8p/603s1.jpg',
+    },
+
+    {
+        name: 'Bor Bence',
+        best: '',
+        gender: 'férfi',
+        tittle: 'jétékos',
+        img: 'https://www.fradi.hu/wm/upload/295x393/a/hz/ycuww.jpg',
+    },
+
+    {
+        name: 'Deák Dániel',
+        best: '',
+        gender: 'férfi',
+        tittle: 'jétékos',
+        img: 'https://www.fradi.hu/wm/upload/295x393/3/mq/e3x98.jpg',
+    },
+
+    {
+        name: 'Hajas Gábor',
+        best: '',
+        gender: 'férfi',
+        tittle: 'jétékos',
+        img: 'https://www.fradi.hu/wm/upload/295x393/y/5b/qs3ht.jpg',
+    },
+
+    {
+        name: 'Horváth Balázs',
+        best: '',
+        gender: 'férfi',
+        tittle: 'jétékos',
+        img: 'https://www.fradi.hu/wm/upload/295x393/h/hz/4l419.jpg',
+    },
+
+    {
+        name: 'Márkus László',
+        best: '',
+        gender: 'férfi',
+        tittle: 'jétékos',
+        img: 'https://www.fradi.hu/wm/upload/295x393/s/dl/nuppo.jpg',
+    },
+
+    {
+        name: 'Sidó László Kálmán',
+        best: '',
+        gender: 'férfi',
+        tittle: 'jétékos',
+        img: 'https://www.fradi.hu/wm/upload/295x393/9/f3/c65q0.jpg',
+    },
+
+    {
+        name: 'Várvölgyi Milán',
+        best: '',
+        gender: 'férfi',
+        tittle: 'jétékos',
+        img: 'https://www.fradi.hu/wm/upload/295x393/t/gb/j7r1o.jpg',
+    },
+
+    {
+        name: 'Vereckei Zsolt',
+        best: '',
+        gender: 'férfi',
+        tittle: 'jétékos',
+        img: 'https://www.fradi.hu/wm/upload/295x393/s/26/x01n1.jpg',
+    },
+
+    {
+        name: 'Vígh István Attila',
+        best: '',
+        gender: 'férfi',
+        tittle: 'jétékos',
+        img: 'https://www.fradi.hu/wm/upload/295x393/w/gh/z1jtd.jpg',
+    },
+
+    {
+        name: 'Mátraházi István',
+        best: '',
+        gender: 'férfi',
+        tittle: 'szakosztályvezető',
+        img: 'https://www.fradi.hu/wm/upload/295x393/a/cy/ms3ep.jpg',
+    },
+
 ]
 
 export {
     name,
     best,
     gender,
-    image
+    img
 }
