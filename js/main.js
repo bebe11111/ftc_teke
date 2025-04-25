513503afaa14df3c905bc5166699f3259010f12f
Bor Bence <132979209+bebe11111@users.noreply.github.com>
2025-04-25 18:59:31 +0200

    cardfill_js_import

--- js/main.js
+++ js/main.js
@@ -2,5 +2,25 @@ import{
     name,
     best,
     gender,
-    image
+    tittle,
+    img
 }   from './data';
+
+const div = document.querySelector('#cards')
+
+for(const player in players)
+{
+    const card = document.createElement("div")
+    const h5 = document.createElement("h5")
+    const image = document.createElement("img")
+
+    card.classList.add('card')
+
+    h5.textContent = player.name
+
+    image.src = player.img
+    image.alt = img.tittle = player.name
+
+    card.append("h5", "image")
+    div.append("card")
+}
