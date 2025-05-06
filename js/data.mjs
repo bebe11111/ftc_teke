3535d6df5275d8b214cb586b7b31d184f5b0e098
Bor Bence <132979209+bebe11111@users.noreply.github.com>
2025-05-06 22:47:31 +0200

    data_final

--- js/data.mjs
+++ js/data.mjs
@@ -3,147 +3,147 @@
 const players = [
     {
         name: 'Bodó Bettina',
-        best: '',
+        best: '557',
         gender: 'nő',
         img: 'https://www.fradi.hu/wm/upload/295x393/6/nr/hh647.jpg',
     },
 
     {
         name: 'Bognár Barbara',
-        best: '',
+        best: '559',
         gender: 'nő',
         img: 'https://www.fradi.hu/wm/upload/295x393/z/a4/0li6u.jpg',
     },
 
     {
         name: 'Fábián Adelin',
-        best: '',
+        best: '605',
         gender: 'nő',
         img: 'https://www.fradi.hu/wm/upload/295x393/u/01/1vbia.jpg',
     },
 
     {
         name: 'Heimann Ibolya',
-        best: '',
+        best: '598',
         gender: 'nő',
         img: 'https://www.fradi.hu/wm/upload/295x393/9/r8/9xmb6.jpg',
     },
 
     {
         name: 'Mátraháziné Kiss Julianna',
-        best: '',
+        best: '565',
         gender: 'nő',
         img: 'https://www.fradi.hu/wm/upload/295x393/z/xu/z93g6.jpg',
     },
 
     {
         name: 'Nagy Beatrix',
-        best: '',
+        best: '607',
         gender: 'nő',
         img: 'https://www.fradi.hu/wm/upload/295x393/g/hg/v8b2f.jpg',
     },
 
     {
         name: 'Pintér Barbara',
-        best: '',
+        best: '606',
         gender: 'nő',
         img: 'https://www.fradi.hu/wm/upload/295x393/f/w9/0xdu5.jpg',
     },
 
     {
         name: 'Sinka Cintia Gréta',
-        best: '',
+        best: '594',
         gender: 'nő',
         img: 'https://www.fradi.hu/wm/upload/295x393/e/58/2rcqs.jpg',
     },
 
     {
         name: 'Szabó Mónika',
-        best: '',
+        best: '592',
         gender: 'nő',
         img: 'https://www.fradi.hu/wm/upload/295x393/j/70/vdz7y.jpg',
     },
 
     {
         name: 'Tóth Andrea',
-        best: '',
+        best: '603',
         gender: 'nő',
         img: 'https://www.fradi.hu/wm/upload/295x393/2/sx/qf36f.jpg',
     },
 
     {
         name: 'Bánhegyi János',
-        best: '',
+        best: '598',
         gender: 'férfi',
         img: 'https://www.fradi.hu/wm/upload/295x393/j/8p/603s1.jpg',
     },
 
     {
         name: 'Bor Bence',
-        best: '',
+        best: '580',
         gender: 'férfi',
         img: 'https://www.fradi.hu/wm/upload/295x393/a/hz/ycuww.jpg',
     },
 
     {
         name: 'Deák Dániel',
-        best: '',
+        best: '618',
         gender: 'férfi',
         img: 'https://www.fradi.hu/wm/upload/295x393/3/mq/e3x98.jpg',
     },
 
     {
         name: 'Hajas Gábor',
-        best: '',
+        best: '596',
         gender: 'férfi',
         img: 'https://www.fradi.hu/wm/upload/295x393/y/5b/qs3ht.jpg',
     },
 
     {
         name: 'Horváth Balázs',
-        best: '',
+        best: '592',
         gender: 'férfi',
         img: 'https://www.fradi.hu/wm/upload/295x393/h/hz/4l419.jpg',
     },
 
     {
         name: 'Márkus László',
-        best: '',
+        best: '603',
         gender: 'férfi',
         img: 'https://www.fradi.hu/wm/upload/295x393/s/dl/nuppo.jpg',
     },
 
     {
         name: 'Sidó László Kálmán',
-        best: '',
+        best: '635',
         gender: 'férfi',
         img: 'https://www.fradi.hu/wm/upload/295x393/9/f3/c65q0.jpg',
     },
 
     {
         name: 'Várvölgyi Milán',
-        best: '',
+        best: '625',
         gender: 'férfi',
         img: 'https://www.fradi.hu/wm/upload/295x393/t/gb/j7r1o.jpg',
     },
 
     {
         name: 'Vereckei Zsolt',
-        best: '',
+        best: '563',
         gender: 'férfi',
         img: 'https://www.fradi.hu/wm/upload/295x393/s/26/x01n1.jpg',
     },
 
     {
         name: 'Vígh István Attila',
-        best: '',
+        best: '573',
         gender: 'férfi',
         img: 'https://www.fradi.hu/wm/upload/295x393/w/gh/z1jtd.jpg',
     },
 
     {
         name: 'Mátraházi István',
-        best: '',
+        best: '554',
         gender: 'férfi',
         img: 'https://www.fradi.hu/wm/upload/295x393/a/cy/ms3ep.jpg',
     },
