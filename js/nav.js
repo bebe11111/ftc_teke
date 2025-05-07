67a77ced95fdc4198a0a7016d5e4e61434f89266
Bor Bence <132979209+bebe11111@users.noreply.github.com>
2025-05-07 16:20:40 +0200

    nav_updt_v2

--- js/nav.js
+++ js/nav.js
@@ -3,3 +3,7 @@
 document.querySelector('#nav-toggle').addEventListener('click', () => {
     document.querySelector('nav').classList.toggle('closed');
 });
+
+document.querySelectorAll('.nav-option').addEventListener('click', () => {
+    document.querySelector('nav').classList.toggle('closed');
+});
