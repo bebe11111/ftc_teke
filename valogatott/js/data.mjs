7cf5343b641bb44d5b04a37badce68ef2ceac99f
Bor Bence <132979209+bebe11111@users.noreply.github.com>
2025-05-08 22:13:30 +0200

    js

---
+++ valogatott/js/data.mjs
@@ -0,0 +1,155 @@
+"use strict"
+
+const players = [
+    {
+        name: 'Bodó Bettina',
+        best: '557',
+        gender: 'nő',
+        img: 'https://www.fradi.hu/wm/upload/295x393/6/nr/hh647.jpg',
+    },
+
+    {
+        name: 'Bognár Barbara',
+        best: '559',
+        gender: 'nő',
+        img: 'https://www.fradi.hu/wm/upload/295x393/z/a4/0li6u.jpg',
+    },
+
+    {
+        name: 'Fábián Adelin',
+        best: '609',
+        gender: 'nő',
+        img: 'https://www.fradi.hu/wm/upload/295x393/u/01/1vbia.jpg',
+    },
+
+    {
+        name: 'Heimann Ibolya',
+        best: '598',
+        gender: 'nő',
+        img: 'https://www.fradi.hu/wm/upload/295x393/9/r8/9xmb6.jpg',
+    },
+
+    {
+        name: 'Mátraháziné Kiss Julianna',
+        best: '565',
+        gender: 'nő',
+        img: 'https://www.fradi.hu/wm/upload/295x393/z/xu/z93g6.jpg',
+    },
+
+    {
+        name: 'Nagy Beatrix',
+        best: '607',
+        gender: 'nő',
+        img: 'https://www.fradi.hu/wm/upload/295x393/g/hg/v8b2f.jpg',
+    },
+
+    {
+        name: 'Pintér Barbara',
+        best: '606',
+        gender: 'nő',
+        img: 'https://www.fradi.hu/wm/upload/295x393/f/w9/0xdu5.jpg',
+    },
+
+    {
+        name: 'Sinka Cintia Gréta',
+        best: '594',
+        gender: 'nő',
+        img: 'https://www.fradi.hu/wm/upload/295x393/e/58/2rcqs.jpg',
+    },
+
+    {
+        name: 'Szabó Mónika',
+        best: '592',
+        gender: 'nő',
+        img: 'https://www.fradi.hu/wm/upload/295x393/j/70/vdz7y.jpg',
+    },
+
+    {
+        name: 'Tóth Andrea',
+        best: '603',
+        gender: 'nő',
+        img: 'https://www.fradi.hu/wm/upload/295x393/2/sx/qf36f.jpg',
+    },
+
+    {
+        name: 'Bánhegyi János',
+        best: '598',
+        gender: 'férfi',
+        img: 'https://www.fradi.hu/wm/upload/295x393/j/8p/603s1.jpg',
+    },
+
+    {
+        name: 'Bor Bence',
+        best: '580',
+        gender: 'férfi',
+        img: 'https://www.fradi.hu/wm/upload/295x393/a/hz/ycuww.jpg',
+    },
+
+    {
+        name: 'Deák Dániel',
+        best: '618',
+        gender: 'férfi',
+        img: 'https://www.fradi.hu/wm/upload/295x393/3/mq/e3x98.jpg',
+    },
+
+    {
+        name: 'Hajas Gábor',
+        best: '596',
+        gender: 'férfi',
+        img: 'https://www.fradi.hu/wm/upload/295x393/y/5b/qs3ht.jpg',
+    },
+
+    {
+        name: 'Horváth Balázs',
+        best: '592',
+        gender: 'férfi',
+        img: 'https://www.fradi.hu/wm/upload/295x393/h/hz/4l419.jpg',
+    },
+
+    {
+        name: 'Márkus László',
+        best: '603',
+        gender: 'férfi',
+        img: 'https://www.fradi.hu/wm/upload/295x393/s/dl/nuppo.jpg',
+    },
+
+    {
+        name: 'Sidó László Kálmán',
+        best: '635',
+        gender: 'férfi',
+        img: 'https://www.fradi.hu/wm/upload/295x393/9/f3/c65q0.jpg',
+    },
+
+    {
+        name: 'Várvölgyi Milán',
+        best: '625',
+        gender: 'férfi',
+        img: 'https://www.fradi.hu/wm/upload/295x393/t/gb/j7r1o.jpg',
+    },
+
+    {
+        name: 'Vereckei Zsolt',
+        best: '563',
+        gender: 'férfi',
+        img: 'https://www.fradi.hu/wm/upload/295x393/s/26/x01n1.jpg',
+    },
+
+    {
+        name: 'Vígh István Attila',
+        best: '573',
+        gender: 'férfi',
+        img: 'https://www.fradi.hu/wm/upload/295x393/w/gh/z1jtd.jpg',
+    },
+
+    {
+        name: 'Mátraházi István',
+        best: '554',
+        gender: 'férfi',
+        img: 'https://www.fradi.hu/wm/upload/295x393/a/cy/ms3ep.jpg',
+    },
+
+]
+
+export {
+    players
+}
---
+++ valogatott/js/main.js
@@ -0,0 +1,160 @@
+"use strict"
+
+import{
+    players
+}   from './data.mjs';
+
+const div = document.querySelector('#cards')
+
+let updatePlayer = -1;
+
+let deletePlayer = -1;
+
+const updateDialog = document.querySelector('#update-dialog')
+
+const deleteDialog = document.querySelector('#delete-dialog')
+
+function generateParagraph(text){
+    const p = document.createElement('p')
+    p.classList.add('flex','flex-col','justify-around',) //nincsen középen a szöveg
+    p.textContent = text
+    return p
+}
+
+function generatecard(){
+    const div = document.querySelector('#cards')
+    div.replaceChildren()
+
+    for(const [index, player] of players.entries())
+        {
+            const card = document.createElement("div")
+            const image = document.createElement("img")
+            const grid = document.createElement('div')
+            
+            grid.classList.add('grid','grid-cols-2','m-5','mx-auto','gap-1',)
+
+            grid.append(
+                generateParagraph(player.name),
+                generateEditButton(index),
+                generateParagraph("Legjobb eredmény: " +  player.best),
+                generateDelButton(index),
+
+            )
+        
+            card.classList.add('card')
+        
+            image.src = player.img
+            image.alt = image.tittle = player.name
+        
+            card.append(image, grid)
+            
+            div.append(card)
+            
+        }
+        
+}
+
+generatecard();
+
+
+function generateDelButton(idx) {
+    const button = document.createElement('button');
+    const div = document.createElement('div')
+    button.textContent = "Törlés"
+    button.classList.add('bg-red-500','p-2', 'rounded', 'cursor-pointer',);
+    button.addEventListener('click', () => {
+        deletePlayer = idx;
+        const items = [];
+        for (const key in players[idx]) {
+            const li = document.createElement('li')
+            li.classList.add('border', 'border-green-800', 'rounded','m-3', 'min-h-[2rem]')
+            li.textContent = players[idx][key];
+            items.push(li);
+        }
+        deleteDialog.querySelector('ul').replaceChildren(...items);
+        deleteDialog.showModal();
+    });
+    div.classList.add('p-2')
+    div.append(button)
+    return div
+}
+
+function generateEditButton(idx) {
+    const button = document.createElement('button');
+    const div = document.createElement('div')
+    button.textContent = "Szerkesztés"
+    button.classList.add('bg-yellow-500', 'p-2', 'rounded', 'cursor-pointer');
+    button.addEventListener('click', () => {
+        updatePlayer = idx;
+        for (const key in players[idx]) {
+            updateDialog.querySelector(`[name="${key}"]`).value = players [idx] [key];
+        }
+        updateDialog.showModal();
+    });
+    div.classList.add('p-2')
+    div.append(button)
+    return div
+}
+
+document.querySelector('#create').addEventListener('submit', event =>{
+    event.preventDefault()
+
+    const name = document.querySelector('#name').value;
+    const best = document.querySelector('#best').value;
+    const gender = document.querySelector('#gender').value;
+    const img = document.querySelector('#img').value;
+
+    players.push({
+        name,
+        best,
+        gender,
+        img,
+    });
+
+    generatecard();
+
+    event.target.reset();
+});
+
+document.querySelector('#update').addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector('#update-name').value;
+    const best = document.querySelector('#update-best').value;
+    const gender = document.querySelector('#update-gender').value;
+    const img = document.querySelector('#update-img').value;
+
+    players.splice(updatePlayer, 1, {
+        name,
+        best,
+        gender,
+        img,
+    })
+
+    generatecard();
+    updatePlayer = -1;
+    updateDialog.close();
+    event.target.reset();
+});
+
+document.querySelector('#close-update').addEventListener('click', () => {
+    updatePlayer = -1;
+    updateDialog.close();
+});
+
+document.querySelector('#delete').addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    players.splice(deletePlayer, 1)
+
+    generatecard();
+
+    deletePlayer = -1;
+
+    deleteDialog.close();
+})
+
+document.querySelector('#close-delete').addEventListener('click', () => {
+    updatePlayer = -1;
+    deleteDialog.close();
+});
