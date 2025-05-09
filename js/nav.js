d9e1188e6109af80b0d8fa5ee91f5ebb261e7fd2
Bor Bence <132979209+bebe11111@users.noreply.github.com>
2025-05-09 12:27:06 +0200

    nav_better

--- js/nav.js
+++ js/nav.js
@@ -7,3 +7,7 @@ document.querySelector('#nav-toggle').addEventListener('click', () => {
 document.querySelector('ul').addEventListener('click', () => {
     document.querySelector('nav').classList.toggle('closed');
 });
+
+document.querySelector('#nav-home').addEventListener('click', () => {
+    document.querySelector('nav').classList.add('closed');
+});
